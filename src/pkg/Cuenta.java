f79132b9c6e75c152929e05ebf2721b0c0204795
USUARIO <USUARIO@DESKTOP-RTMH8NF.mshome.net>
2024-10-11 10:33:17 +0200

    2º commit CuentaTest: hacer que testIngresar funcione

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,23 @@
+package pkg;
+
+public class Cuenta {
+	
+	Double saldo;
+	
+	public Cuenta(Double i) {
+		
+	}
+	
+	public Double getSaldo() {
+		return 1d;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void ingresar(Double i) {
+		
+	}
+
+}
