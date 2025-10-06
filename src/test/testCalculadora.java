354ea31c4794b7e8894406afc3745d53aa473c70
usuario <usuario@172.17.22.46>
2025-10-06 18:24:54 +0200

    TDD 1º paso. Implementar pruebas

---
+++ src/test/testCalculadora.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testCalculadora {
+
+	@Test
+	void testSuma() {
+		assertEquals(5, Calculadora.suma(2, 3));
+	}
+
+	@Test
+	void testResta() {
+		assertEquals(1, Calculadora.resta(4, 3));
+	}
+	
+	@Test
+	void testMultiplica() {
+		assertEquals(6, Calculadora.multiplica(2, 3));
+	}
+	
+	@Test
+	void testDivide() {
+		assertEquals(2, Calculadora.divide(6, 3));
+	}
+}
