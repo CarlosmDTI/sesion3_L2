dcb01c18f4c764811c44ba275ab33e04d95d2097
usuario <usuario@172.17.22.46>
2025-10-06 18:45:12 +0200

    2º paso Cuenta. Pruebas pasan

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,50 @@
+package pkg;
+
+public class Cuenta {
+	
+	String numero;
+	String titular;
+	Double saldo;
+	
+	public Cuenta(double i) {
+		// TODO Auto-generated constructor stub
+		this.saldo = i;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void ingresar(double i) {
+		// TODO Auto-generated method stub
+		saldo = 200.0;
+		
+	}
+
+	public void retirar(double i) {
+		// TODO Auto-generated method stub
+		saldo = -200.0;
+	}
+	
+	
+}
